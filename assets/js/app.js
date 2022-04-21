e614155fc0377561805d752e5a0110f471b31bf5
78741722Ale <pecorillaalessandro@gmail.com>
2022-04-21 14:10:47 +0200

    Aggiunto immagini e struttura dati all'app js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,38 @@ const app = new Vue({
     // Riferimento all'container con id App
     el: '#app',
     // dati contenuti
-    data: {},
+    data: {
+        countries: [
+            // Object: Svezia
+            {
+                image: './assets/img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            // Object: Svizzera <3
+            {
+                image: './assets/img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            // Object: Inghilterra
+            {
+                image: './assets/img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            // Object: Germania
+            {
+                image: './assets/img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            // Object: Paradiso
+            {
+                image: './assets/img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+        ]
+    },
 })
